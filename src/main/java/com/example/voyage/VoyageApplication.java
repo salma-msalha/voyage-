cad772ef8b8ad3382b3529d57d9780f82220f565
Salma <salmamsalha23@gmail.com>
2023-06-26 22:30:47 +0100

    Espace Admin

--- src/main/java/com/example/voyage/VoyageApplication.java
+++ src/main/java/com/example/voyage/VoyageApplication.java
@@ -2,13 +2,15 @@ package com.example.voyage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.Controller"})
 public class VoyageApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VoyageApplication.class, args);
-		System.out.println("test salma");
+		
 	}
 
 }
